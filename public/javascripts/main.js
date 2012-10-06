5b054ece75cc5847e3f3ac0155822319bb9b057b
Luke Stebner <luke.stebner@gmail.com>
2012-10-06 00:47:58 -0700

    added select language to UI, not hooked up yet

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 //@codekit-prepend "jquery-1.8.2.js";
+//@codekit-prepend "bootstrap/bootstrap-dropdown.js";
 //@codekit-prepend "underscore-1.3.3.js";
 //@codekit-prepend "backbone-0.9.2.js";
 //@codekit-prepend "shCore.js";
